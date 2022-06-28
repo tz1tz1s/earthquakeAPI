2e8e68709e869626fe4ef09c5b0490bc90393bba
karag42 <61845456+karag42@users.noreply.github.com>
2022-06-29 00:08:25 +0300

    Update api_get_from_database.py
    final state

--- api/api_get_from_database.py
+++ api/api_get_from_database.py
@@ -1,44 +1,46 @@
 import json
 
-from flask import Flask,redirect,request,jsonify
+from flask import Flask, redirect, request, jsonify
 
 import mysql.connector
 
-app =  Flask(__name__)
+app = Flask(__name__)
 
 mydb = mysql.connector.connect(
-  host="127.0.0.1",
-  user="root",
-  password="",
-  database="final_db"
+    host="127.0.0.1",
+    user="root",
+    password="",
+    database="final_db"
 )
 mycursor = mydb.cursor()
+
 
 @app.route('/')
 def hello():
     print("Hello World")
     return "hello world"
 
-@app.route("/get_data",methods=['GET','POST'])
 
+@app.route("/get_data", methods=['GET', 'POST'])
 def get_data_from_database():
-
-
     get_data_sql = """
-    SELECT JSON_OBJECT ('id', ID,'magnitude', magnitude,'timestamp', time_stamp,'Place',City) FROM all_in_one;
-    """
+     SELECT JSON_OBJECT ('id', ID,'magnitude', magnitude,'timestamp', time_stamp,'Place',City) FROM all_in_one;
+     """
     mycursor.execute(get_data_sql)
     data = mycursor.fetchall()
     length = len(data)
     print(length)
-    final_json_object = {}
+    final_json_object = ""
     for i in range(length):
         print(data[i][0])
-        final_json_object[i] = data[i][0]
+        final_json_object += data[i][0]
+
+
 
     return str(final_json_object)
 
 
 
+
 if __name__ == '__main__':
-    app.run(port = "5003")
+    app.run(port="5003")
