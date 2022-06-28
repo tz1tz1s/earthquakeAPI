c4fdedd065c588a80dfa56d9ae78c57a75b276c2
karag42 <61845456+karag42@users.noreply.github.com>
2022-06-29 00:56:30 +0300

    Update api_post_to_database.py
    corrected mistake

--- api/api_post_to_database.py
+++ api/api_post_to_database.py
@@ -1,56 +1,47 @@
-import json
-
-from flask import Flask, redirect, request, jsonify
-
+from flask import Flask,redirect,request
 import mysql.connector
 
-app = Flask(__name__)
+app =  Flask(__name__)
 
 mydb = mysql.connector.connect(
-    host="127.0.0.1",
-    user="root",
-    password="",
-    database="final_db"
+  host="",
+  user="s58198",
+  password="116396",
+  database="s58198"
 )
-mycursor = mydb.cursor()
 
 
 @app.route('/')
-def hello():
-    print("Hello World")
+
+def hello_world():
     return "hello world"
 
 
-@app.route("/get_data_length", methods=['GET', 'POST'])
-def get_data_from_database():
+@app.route('/api/get/', methods=['GET','POST'])
+
+def get_data_from_backend():
+    if request.method == 'POST':
+        time_p = request.form["time"]
+        magnitude_p = request.form["mag"]
+        place_p = request.form["place"]
+
+
+
+    time_post = time_p
+    magnitude_post = magnitude_p
+    place_post =  place_p
     mycursor = mydb.cursor()
-
-    mycursor.execute("SELECT id FROM all_in_one" )
-    myresult = mycursor.fetchall()
-    print(jsonify(myresult))
-    num_of_id = len(myresult)
-    return str(num_of_id)
+    final_sql_statement2 = """ INSERT INTO all_in_one (time_stamp,magnitude,City) VALUES (%s,%s,%s)"""
+    entry_tuple = (time_post, magnitude_post, place_post)
 
 
-@app.route("/get_data_by_id/<int:ID>", methods=['GET'])
+    mycursor.execute(final_sql_statement2, entry_tuple)
+    mydb.commit()
 
-def return_data_by_id(ID):
-    mycursor = mydb.cursor()
-    #mycursor.execute("SELECT * FROM all_in_one WHERE ID=" + str(ID))
-    mycursor.execute("SELECT * FROM all_in_one ")
-    myresult = mycursor.fetchall()
-    return jsonify(myresult)
-
-
-@app.route("/get_data_mag/<int:magn>", methods=['GET'])
-
-def return_data_by_magnitude(magn):
-    mycursor = mydb.cursor()
-    mycursor.execute("SELECT * FROM all_in_one WHERE magnitude >" + str(magn))
-    myresult = mycursor.fetchall()
-    return jsonify(myresult)
+    print("data posted")
+    return "data posted"
 
 
 
 if __name__ == '__main__':
-    app.run(port="5006")
+    app.run(port = 5002)
