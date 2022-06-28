6a5fcd19a3d94ea295347afc5f2f6504774a9790
PanosGoug <105055336+PanosGoug@users.noreply.github.com>
2022-06-29 00:43:33 +0300

    The final version
    The complete version of the Front end python code.

--- Front-end_main.py
+++ Front-end_main.py
@@ -1,81 +1,70 @@
 from flask import Flask
 from flask import request, render_template
-from flask_restful import Api, Resource
-import mysql.connector
+from flask_restful import Api
 import json
 import requests
-from datetime import datetime
-import self
-import urllib.parse as urlparse
-from urllib.parse import parse_qs
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from splinter import Browser
+
 
 
 app = Flask(__name__)
-api= Api(app)
+api = Api(app)
 
-mydb = mysql.connector.connect(
-    host="dalab.ee.duth.gr",
-    user='s58198',
-    password="116396",
-    database='s58198'
-)
 
 @app.route("/")
 def start():
 
     return render_template('MainPage.html')
 
-@app.route("/2")
-def Search():
-    #mycursor1 = mydb.cursor()
-    #mycursor1.execute("SELECT City FROM Cities;")
-    #myresults1 = mycursor1.fetchall()
-    #mycursor2 = mydb.cursor()
-    #mycursor2.execute("SELECT Mag FROM Magnitude;")
-    #myresults2 = mycursor1.fetchall()
-    #mycursor3 = mydb.cursor()
-    #mycursor3.execute("SELECT Date FROM Date;")
-    #myresults3 = mycursor1.fetchall()
-    #mycursor4 = mydb.cursor()
-    #mycursor4.execute("SELECT Id FROM Cities;")
-    #myresults4 = mycursor1.fetchall()
-    #num = len(myresults1)
-    #return render_template('SearchWindow.html', list_header='Results:', Cities=myresults1, Mag=myresults2, Date=myresults3, Id=myresults4, test=num)
-    responce = json.loads(requests.get("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson").text)
+@app.route("/Search_results/<int:x>")
+def Search(x):
+    length = int(requests.get("http://127.0.0.1:5006/get_data_length").text)
     City = []
     Mag = []
-    Date= []
-    num = len(responce["features"])
-    for i in range(len(responce["features"])):
-        City.append(responce["features"][i]["properties"]["place"])
-        Mag.append(responce["features"][i]["properties"]["mag"])
-        temp = int(responce["features"][i]["properties"]["time"])
-        timestamp = temp / 1000
-        time = datetime.fromtimestamp(timestamp).strftime('%d-%m-%y')
-        Date.append(time)
-    return render_template('SearchWindow.html', list_header = 'Results',Cities=City, Mag=Mag, Date=Date, test=num)
+    time = []
+    if x==0:
+        for i in range(length):
+            url1= "http://127.0.0.1:5006/get_data_by_id/"+str(i+1)
+            data = json.loads(requests.get(url1).text)
+            City.append(data[3])
+            Mag.append(data[2])
+            time.append(data[1])
+        return render_template('SearchWindow.html', list_header='Results', Cities=City, Mag=Mag, Date=time, test=length)
 
-@app.route("/3/<int:x>")
-def all_results(x):
-    responce = json.loads(
-        requests.get('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson').text)
+    if x==1:
+        mag = request.args.get("Magnitude")
+        url2= "http://127.0.0.1:5006/get_data_mag/"+str(mag)
+        data = json.loads(requests.get(url2).text)
+        length= len(data)
+        for i in range(length):
+            City.append(data[i][3])
+            Mag.append(data[i][2])
+            time.append(data[i][1])
+        return render_template('SearchWindowDiff.html', list_header = 'Results',Cities=City, Mag=Mag, Date=time, test=length,num=mag)
+    return ("Something went wrongh")
+
+@app.route("/Details/<int:x>/<int:y>")
+def details(x, y):
+    length = int(requests.get("http://127.0.0.1:5006/get_data_length").text)
     City = []
     Mag = []
-    Date = []
-    types= []
-    num = len(responce["features"])
-    City.append(responce["features"][x]["properties"]["place"])
-    Mag.append(responce["features"][x]["properties"]["mag"])
-    temp = int(responce["features"][x]["properties"]["time"])
-    types.append(responce["features"][x]["properties"]["types"])
-    timestamp = temp / 1000
-    time = datetime.fromtimestamp(timestamp).strftime('%d-%m-%y')
-    Date.append(time)
-    return render_template('Details.html', list_header='Results', Cities=City, Mag=Mag, Date=Date, test=num, Types=types)
+    time = []
+    if x == 0:
+        url1 = "http://127.0.0.1:5006/get_data_by_id/" + str(y+1)
+        data = json.loads(requests.get(url1).text)
+        City.append(data[3])
+        Mag.append(data[2])
+        time.append(data[1])
+    responce = json.loads(requests.get('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson').text)
+    if x==1:
+        mag = request.args.get("Magnitude")
+        url2= "http://127.0.0.1:5006/get_data_mag/"+str(mag)
+        data = json.loads(requests.get(url2).text)
+        length= len(data)
+        City.append(data[y][3])
+        Mag.append(data[y][2])
+        time.append(data[y][1])
+    return render_template('Details.html', list_header = 'Results',Cities=City, Mag=Mag, Date=time, test=length)
 
 
 if __name__ == '__main__':
-    app.run(port=5002)
+    app.run(port=5004)
